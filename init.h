687c49e2d1533829e673b240751172807c4bf3ee
Kundan Kadam <kkadam1@tigers.lsu.edu>
2014-02-12 16:15:35 -0600

    First steps

---
+++ init.h
@@ -0,0 +1,11 @@
+!Binary parameters
+       integer, parameter :: ra = 168
+       integer, parameter :: rb = 18
+       integer, parameter :: rc = 22
+       integer, parameter :: rd = 110
+       real, parameter :: rhom1 = 1.0
+       real, parameter :: rhom2 = 0.6
+       
+       integer, parameter :: type = 1     
+       integer, parameter :: model_num = 1000
+       integer, parameter :: iam = 0
